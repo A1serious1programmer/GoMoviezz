d044cac31e57ec2f20f8ead6fc7af878d8ff1ef5
Ojas_ <114296963+ojas-sharma-01@users.noreply.github.com>
2024-07-12 16:24:00 +0530

    Update scripttv.js

--- scripttv.js
+++ scripttv.js
@@ -1,14 +1,16 @@
 document.querySelector(".button").addEventListener("click", function(){show_det(event, document.querySelector("input").value);
 });
 
-fetch("https://imdb-api.com/en/API/MostPopularTVs/k_1d5br83e")
+fetch("https://imdb-top-100-movies.p.rapidapi.com/", {
+  headers : {"x-rapidapi-key": "92d36c2446msh1a0c7162cd192dfp17f1d9jsnba38a9805a6a"}
+  })
   .then(response => response.json())
   .then(data => {
     console.log(data);
     for(let i = 0 ; i < 20 ; i++){
-      if (data.items[i].image != ''){
+      if (data[i].image != ''){
         all_post[i].classList.remove("hide");
-        all_post[i].src = (data.items[i].image);
+        all_post[i].src = (data[i].image);
     }}});
 
 const all_post = document.querySelectorAll(".movies div img");
