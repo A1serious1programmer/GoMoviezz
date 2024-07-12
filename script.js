1e40c9ec07b11c2ce4f205b8abdfd2887b844e10
Ojas_ <114296963+ojas-sharma-01@users.noreply.github.com>
2024-07-12 16:23:03 +0530

    Update script.js

--- script.js
+++ script.js
@@ -3,20 +3,22 @@
 document.querySelector(".button").addEventListener("click", function(){show_det(event, document.querySelector("input").value);
 });
 
-fetch("https://imdb-api.com/en/API/MostPopularMovies/k_1d5br83e")
+fetch("https://imdb-top-100-movies.p.rapidapi.com/", {
+  headers : {"x-rapidapi-key": "92d36c2446msh1a0c7162cd192dfp17f1d9jsnba38a9805a6a"}
+})
   .then(response => response.json())
   .then(data => {
     console.log(data);
     for(let i = 0 ; i < 20 ; i++){
-      if (data.items[i].image != ''){
+      if (data[i].image != ''){
         all_post[i].classList.remove("hide");
-        all_post[i].src = (data.items[i].image);
+        all_post[i].src = (data[i].image);
     }}});
 
 const all_post = document.querySelectorAll(".movies div img");
 function show_det(event, movie){
   event.preventDefault();
-  fetch("https://imdb-api.com/en/API/SearchMovie/k_1d5br83e/"+movie)
+  fetch("https://imdb-top-100-movies.p.rapidapi.com/"+movie)
     .then(response => response.json())
     .then(data => {
       var count = 0;
