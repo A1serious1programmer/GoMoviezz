c4e2fd17c1bc3558acbff18df308327e609c9b46
Ojas_ <114296963+ojas-sharma-01@users.noreply.github.com>
2024-07-12 16:21:39 +0530

    Update script1.js

--- script1.js
+++ script1.js
@@ -10,6 +10,7 @@ fetch("https://imdb-top-100-movies.p.rapidapi.com/", {
 
 fetch("https://imdb-top-100-movies.p.rapidapi.com/", {
     headers : {"x-rapidapi-key": "92d36c2446msh1a0c7162cd192dfp17f1d9jsnba38a9805a6a"}
+}
   )
     .then(response => response.json())
     .then(data => {
